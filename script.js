7dbd042aaba6f65d0032c3575562f55fd15a4518
Enno Boland <g@s01.de>
2015-05-03 15:12:38 +0200

    correctly indent index.html and script.js

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 (function() {
-var l = window.location;
-var socks = [];
-function setup(ii, addr) {
-	var sock, i, closing = false;
-	if(addr == "")
-		return;
-	else if(addr == '/') {
-		addr = (l.protocol == "https:" ? "wss://" : "ws://") + l.hostname + ":" + l.port;
-	}
-	try {
-		sock = new WebSocket(addr, "ws");
-	} catch(e) {
-		console.log(e);
-		return setTimeout(function() {
-			setup(ii, addr);
-		}, 500);
-	}
-	sock.onmessage = function(ev) {
-		for(i = 0; i < socks.length; i++) {
-			if(i != ii && socks[i] && socks[i].readyState==1)
-				socks[i].send(ev.data);
-		}
-	};
-	sock.onclose = sock.onerror = function() {
-		for(i = 0; i < socks.length; i++) {
-			if(i != ii && socks[i] && socks[i].readyState<=1)
-				socks[i].close();
-		}
-		if(!closing)
-			setup(ii, addr);
-		closing = true;
-	};
-	socks[ii] = sock;
-}
+	var l = window.location;
+	var socks = [];
 
-for(var i = 0; i < arguments.length; i++) {
-	setup(i, arguments[i])
-}
+	function setup(ii, addr) {
+		var sock, i, closing = false;
+		if(addr == "") {
+			return;
+		} else if(addr == '/') {
+			addr = (l.protocol == "https:" ? "wss://" : "ws://") + l.hostname + ":" + l.port;
+		}
+		try {
+			sock = new WebSocket(addr, "ws");
+		} catch(e) {
+			return setTimeout(function() {
+				setup(ii, addr);
+			}, 500);
+		}
+		sock.onmessage = function(ev) {
+			for(i = 0; i < socks.length; i++) {
+				if(i != ii && socks[i] && socks[i].readyState==1)
+					socks[i].send(ev.data);
+			}
+		};
+		sock.onclose = sock.onerror = function() {
+			for(i = 0; i < socks.length; i++) {
+				if(i != ii && socks[i] && socks[i].readyState<=1)
+					socks[i].close();
+			}
+			if(!closing)
+				setup(ii, addr);
+			closing = true;
+		};
+		socks[ii] = sock;
+	}
 
+	for(var i = 0; i < arguments.length; i++) {
+		setup(i, arguments[i]);
+	}
 })("%s", "%s");
